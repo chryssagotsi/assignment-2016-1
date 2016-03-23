1f94dc240a47a125afdb09e1d7b34b856987075b
chryssagotsi <chryssa.gotsi@gmail.com>
2016-03-23 16:33:01 +0200

    Update trusses.py

--- trusses.py
+++ trusses.py
@@ -1,7 +1,7 @@
- open(graph_file, r)
- crummy_code_to_reduce_graph_to_k_truss(graph_file, k)
+ g = open(graph_file, r)
+crummy_code_to_reduce_graph_to_k_truss(g, k)
     until no change do
-        for each edge e = (a,b) in graph_file:
+        for each edge e = (a,b) in g:
             if (size(intersection(neighbours(a), neighbours(b)) < k - 2:
-                remove e from graph_file
+                remove e from g
     return remaining edges for each node \ 
